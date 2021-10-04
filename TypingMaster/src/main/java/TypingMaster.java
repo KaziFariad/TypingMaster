968a33e5c1356e1a7456fb56a5f0510ade6abf4e
KaziFariad <53252317+KaziFariad@users.noreply.github.com>
2021-10-04 18:36:31 +0530

    Initial commit

---
+++ TypingMaster/src/main/java/TypingMaster.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class TypingMaster {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            driver.get("https://10fastfingers.com/typing-test/english");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("row1")));
+            WebElement line = driver.findElement(By.id("row1"));
+            WebElement inputField = driver.findElement(By.id("inputfield"));
+
+//            inputField.sendKeys(line.getAttribute("textContent"));
+
+            String lines = line.getAttribute("textContent");
+            String[] words = lines.split(" ");
+            long startTime = System.currentTimeMillis();
+
+                for (String word : words) {
+                    if((System.currentTimeMillis()-startTime)>60000){
+                        break;
+                    }
+                    for (int i = 0; i < word.length(); i++) {
+                        char c = word.charAt(i);
+                        String s = String.valueOf(c);
+                        inputField.sendKeys(s);
+                        Thread.sleep(40);
+                    }
+                    inputField.sendKeys(" ");
+                }
+            }
+
+        catch(Exception e){
+            System.out.println("Caught exception: " + e.getMessage());
+        }
+    }
+
+}
